1a4f25cf58f4d7eddf3e2f0ce513bfae53f3dc58
Rupon <fardiidlamrupon@gmail.com>
2024-12-05 20:31:45 +0600

    deleted succes from server

--- index.js
+++ index.js
@@ -50,6 +50,14 @@ async function run() {
       res.send(result);
     })
 
+    // delete a single movie
+    app.delete('/allmovie/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await movieCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
 
 
